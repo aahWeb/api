a856daa3570f6f906cdd5a49b4336f45658a0125
Antoine07 <antoine.lucsko@gmail.com>
2023-05-24 14:38:16 +0200

    pagination

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { Express } from 'express';
 import router from "./routes";
+import cors from "cors";
 
-const port :number = 3000;
+const port :number = 3001;
 const app : Express = express();
+
+app.use(cors())
 
 app.use("/api", router);
 
-
-app.listen(3000, () =>
+app.listen(port, () =>
   console.log(`listen http://localhost:${port}`),
 );
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,3 @@
-
 import express, { Router, Request, Response } from "express";
 import { PASTRIES as pastries } from "./mocks";
 import { Pastrie } from "./pastrie";
@@ -6,23 +5,36 @@ import { Pastrie } from "./pastrie";
 const router: Router = express.Router();
 
 // all pastries
-router.get("/all", function (req: Request, res: Response) {
+router.get("/pastries", function (req: Request, res: Response) {
     res.json(pastries);
 });
 
 // id pastries
 router.get("/pastrie/:id", function (req: Request, res: Response) {
-    const id : string = req.params.id
-    const pastrie: Pastrie | undefined = pastries.find(p => p.id == id);
+    const id: string = req.params.id
+    const p: Pastrie | undefined = pastries.find(p => p.id == id);
 
-    if(pastrie)
-         res.json(pastrie);
-
-
+    if (p)
+        res.json(p);
 });
 
-router.get('*',function (req: Request, res: Response) {
-    res.status(404).json({ error : "Not found" })
+/**
+ * Exemple de récupération des données avec start et end dans l'url 
+ * Dans l'exemple ci-dessous on récupère deux pastries 
+ * api/pastries/0/2
+ */
+router.get("/pastries/:start?/:end", function (req: Request, res: Response) {
+    const start: string = req.params.start;
+    const end: string = req.params.end;
+
+    let p: Pastrie[] = end ? pastries.slice(parseInt(start), parseInt(end) + 1) : pastries.slice(parseInt(start))
+
+    if (p)
+        res.json(p);
+});
+
+router.get('*', function (req: Request, res: Response) {
+    res.status(404).json({ error: "Not found" })
 });
 
 export default router;
