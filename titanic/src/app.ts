5abae80c284b39582e2c90e16d40b37ed3d5ac65
Antoine07 <antoine.lucsko@gmail.com>
2023-05-26 10:07:39 +0200

    titanic

--- titanic/src/app.ts
+++ titanic/src/app.ts
@@ -3,12 +3,8 @@ import express, { Express } from 'express';
 
 import cors from "cors";
 import router from './routes';
+import { port, host } from "./config";
 
-import dotenv from 'dotenv'
-dotenv.config()
-
-const port :number | undefined = process.env.API_PORT as number | undefined ;
-const host : string | undefined = process.env.API_URL as string | undefined ;
 const app : Express = express();
 
 app.use(cors());
