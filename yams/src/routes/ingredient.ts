5b4c0615ba3e165b2f9d0a4d61ca87ca9d040fc5
dilaouid <dilaouid@student.42.fr>
2023-06-21 20:51:12 +0200

    complete ingredients & pastries routes

--- yams/src/routes/ingredient.ts
+++ yams/src/routes/ingredient.ts
@@ -1,15 +1,66 @@
 import express, { Router, Request, Response } from "express";
-import { INGREDIENTS_LISTS as ingredients } from "./../mocks";
+import { INGREDIENTS_LISTS } from "./../mocks";
 import { List } from "./../pastrie";
 
 const router: Router = express.Router();
+const ingredients: List[] = INGREDIENTS_LISTS;
+
+router.get('/ingredients', function (req: Request, res: Response) {
+    return res.json(ingredients);
+});
+
+router.put("/ingredient/:id", function (req: Request, res: Response) {
+    const id: string = req.params.id;
+    const list: string[] = req.body.list;
+
+    const i: List | undefined = ingredients.find(i => i.id == id);
+    if (i) {
+        i.list = list;
+        return res.json(i);
+    } else {
+        return res.status(404).json({
+            message: 'Ingrédient non trouvé !'
+        });
+    }
+});
+
+router.post("/ingredient", function (req: Request, res: Response) {
+    try {
+        const list: string[] = req.body;
+        const id: string = (parseInt(ingredients[ingredients.length - 1].id) + 1).toString() || "1";  // get last id and increment it
+        ingredients.push({ id, list });
+        return res.status(201).json(list);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json(error);
+    }
+});
 
 router.get("/ingredient/:id", function (req: Request, res: Response) {
-    const id: string = req.params.id
+    const id: string = req.params.id;
     const i: string[] | undefined = ingredients.find(i => i.id == id)?.list;
+    if (i) {
+        return res.json(i);
+    } else {
+        return res.status(404).json({
+            message: 'Ingrédient non trouvé !'
+        });
+    }
+});
 
-    if (i)
-        res.json(i);
+router.delete("/ingredient/:id", function (req: Request, res: Response) {
+    const id: string = req.params.id;
+    const i: List | undefined = ingredients.find(i => i.id == id);
+    if (i) {
+        ingredients.splice(ingredients.indexOf(i), 1);
+        return res.status(200).json({
+            message: 'Ingrédient supprimé !'
+        });
+    } else {
+        return res.status(404).json({
+            message: 'Ingrédient non trouvé !'
+        });
+    }
 });
 
 export default router;
--- yams/src/routes/pastrie.ts
+++ yams/src/routes/pastrie.ts
@@ -1,11 +1,11 @@
 import express, { Router, Request, Response } from "express";
-import { PASTRIES as pastries } from "./../mocks";
+import { PASTRIES } from "./../mocks";
 import { Pastrie } from "./../pastrie";
 
 const router: Router = express.Router();
 
 // optimisation dans le comptage des pastries 
-const COUNT : number = pastries.length;
+const pastries: Pastrie[] = PASTRIES;
 
 // all pastries
 router.get("/pastries", function (req: Request, res: Response) {
@@ -16,9 +16,14 @@ router.get("/pastries", function (req: Request, res: Response) {
 router.get("/pastrie/:id", function (req: Request, res: Response) {
     const id: string = req.params.id
     const p: Pastrie | undefined = pastries.find(p => p.id == id);
-
-    if (p)
+    
+    if (p) {
         res.json(p);
+    } else {
+        res.status(404).json({
+            message: 'Pâtisserie non trouvée !'
+        });
+    }
 });
 
 router.get("/pastries-search/:word", function (req: Request, res: Response) {
@@ -28,8 +33,13 @@ router.get("/pastries-search/:word", function (req: Request, res: Response) {
     // by quantity order 
     const p: Pastrie[] = pastries.filter(p => p.name.match(re));
 
-    if (p)
+    if (p) {
         res.json(p);
+    } else {
+        res.status(404).json({
+            message: 'Pâtisserie non trouvée !'
+        });
+    }
 });
 
 /**
@@ -37,33 +47,31 @@ router.get("/pastries-search/:word", function (req: Request, res: Response) {
  * Dans l'exemple ci-dessous on récupère deux pastries 
  * api/pastries/0/2
  */
-router.get("/pastries/:start?/:end", function (req: Request, res: Response) {
-    const start: string = req.params.start;
-    const end: string = req.params.end;
+router.get("/pastries/:offset?/:limit", function (req: Request, res: Response) {
+    const offset: number = parseInt(req.params.offset);
+    const limit: number = parseInt(req.params.limit);
 
-    let p: Pastrie[] = end ? pastries.slice(parseInt(start), parseInt(end) + 1) : pastries.slice(parseInt(start))
-
-    if (p)
-        res.json(p);
+    // on commence à offsetr et on prends limit elements
+    const p: Pastrie[] = limit ? pastries.slice(offset).slice(0, limit) : pastries.slice(offset)
+    return res.json(p);
 });
 
 // même requete mais ordonné
-router.get("/pastries/order-quantity/:start?/:end", function (req: Request, res: Response) {
-    const start: string = req.params.start;
-    const end: string = req.params.end;
+router.get("/pastries/order-quantity/:offset?/:limit", function (req: Request, res: Response) {
+    const offset: number = parseInt(req.params.offset);
+    const limit: number = parseInt(req.params.limit);
 
     // by quantity order 
     pastries.sort((a, b) => b.quantity - a.quantity)
 
-    const p: Pastrie[] = end ? pastries.slice(parseInt(start), parseInt(end) + 1) : pastries.slice(parseInt(start))
-
-    if (p)
-        res.json(p);
+    // on commence à offsetr et on prends limit elements
+    const p: Pastrie[] = limit ? pastries.slice(offset).slice(0, limit) : pastries.slice(offset)
+    return res.json(p);
 });
 
 // count number pastries 
 router.get("/pastries-count", function (req: Request, res: Response) {
-    res.json(COUNT);
+    res.json(pastries.length);
 });
 
 router.get('*', function (req: Request, res: Response) {
