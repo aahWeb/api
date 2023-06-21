791b7e5b2fd7f766bb3f976016928a386d33f644
dilaouid <dilaouid@student.42.fr>
2023-06-21 14:55:59 +0200

    get all users route with mongoose

--- docker-api-mongo/app/src/routes/user.ts
+++ docker-api-mongo/app/src/routes/user.ts
@@ -4,7 +4,12 @@ import { IUser, User } from "../db/schemas/UserSchema";
 const router: Router = express.Router();
 
 router.get('/users', function (req: Request, res: Response) {
+    User.find({}).select('id name email').sort({ name: 1 }).then((users: IUser[]) => {
         res.json(users);
+    }).catch((err: Error) => {
+        console.error(err);
+        res.status(500).json(err);
+    });
 });
 
 export default router;
